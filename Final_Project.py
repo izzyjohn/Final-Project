42e725cf83a1d6e3dbb5fe1f6ce8fc6e79b84a7b
Reese salgado <reesesalgado@reeses-MacBook.local>
2022-12-06 11:02:43 -0500

    usa

--- Final_Project.py
+++ Final_Project.py
@@ -2,20 +2,23 @@ import unittest
 import sqlite3
 import json
 import os
-<<<<<<< HEAD
-=======
 import requests
 
 uk_url = 'https://api.coronavirus.data.gov.uk/v1/data'
 india_url = 'https://data.covid19india.org/v4/min/timeseries.min.json'
-us_url = 'https://api.covidtracking.com/v2/us/daily.json
+us_url = 'https://api.covidtracking.com/v2/us/daily.json'
+
+def open_database(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
 
 def read_api(url):
     r = requests.get(url)
     info = r.text
     d = json.loads(info)
     return d
->>>>>>> 2e6819e9a6a10115b70810864d606a9ffe7358d8
 
 
 def uk_data(cur, conn):
@@ -24,5 +27,18 @@ def uk_data(cur, conn):
 def india_data(cur, conn):
     pass
 
-def us_data(cur, conn):
-    pass
+def us_data():
+    data = read_api(us_url)
+    dates = data['data']
+    for date_info in dates:
+        date = date_info['date']
+        total_cases = date_info['cases']['total']['value']
+        new_cases = date_info['cases']['total']['calculated']['change_from_prior_day']
+        total_deaths = date_info['outcomes']['death']['total']['value']
+        new_deaths = date_info['outcomes']['death']['calculated']['change_from_prior_day']
+        current_hospital = date_info['outcomes']['hospitalized']['currently']['value']
+        current_icu = date_info['outcomes']['hospitalized']['in_icu']['currently']['value']
+      
+
+
+
