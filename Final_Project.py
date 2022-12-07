51f073f3495c9df20c76135ddba47b381f8f42a0
Reese salgado <reesesalgado@reeses-MacBook.local>
2022-12-07 11:57:03 -0500

    trying

--- Final_Project.py
+++ Final_Project.py
@@ -69,8 +69,8 @@ def canada_data(api_data, cur, conn):
         total_fatalities, change_fatalities, total_criticals, total_hospitalizations))
     conn.commit()
 
-data = read_api(canada_url)
-print(canada_data(data))
+#data = read_api(canada_url)
+#print(canada_data(data))
 
 def us_data(cur, conn):
     data = read_api(us_url)
